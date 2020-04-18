14724f2009c5e0d1dc5cbfe5d95d055c850099b3
MDreyar <stoeremitch@hotmail.com>
2020-04-18 21:04:43 +0200

    Smoothening of the movement

--- Assets/Scripts/BalanceManager.cs
+++ Assets/Scripts/BalanceManager.cs
@@ -4,17 +4,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEditor;
+using System.ComponentModel;
 
 public class BalanceManager : MonoBehaviour
 {
-    public Slider testSlider;
+    [Header("Debug slider for debug reasons")]
+    public Slider testSliderCurrent;
+    public Slider testSliderTarget;
+
+    [Header("Balancing option")]
+    [Tooltip("How far can you go before you die")]
     public float balanceMax = 50;
     public float minRandomBalanceAjustmentAmount = 3f;
     public float MaxRandomBalanceAjustmentAmount = 15f;
+    [Header("Leaning options")]
+    [Tooltip("This multiplyer is attached to the natural leaning, based on how far the player is already leaning")]
     public float falloverMultiplyer = 1;
+    [Tooltip("How quickly should the player move towards the target. A lower number is quicker!")]
+    public float falloverSpeed = 2;
 
-    [SerializeField]
     public float currentBalance { get; private set; } = 0;
+    public float targetBalance { get; set; } = 0;
+    private float smoothMovementVelocity = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -30,31 +41,35 @@ public class BalanceManager : MonoBehaviour
         // Change balance based on input
         if (Input.GetMouseButtonDown(0))
         {
-            currentBalance += DontGoOverMax(Random.Range(minRandomBalanceAjustmentAmount, MaxRandomBalanceAjustmentAmount));
+            targetBalance += DontGoOverMax(Random.Range(minRandomBalanceAjustmentAmount, MaxRandomBalanceAjustmentAmount));
         }
         else if (Input.GetMouseButtonDown(1))
         {
-            currentBalance += DontGoOverMax(Random.Range(-MaxRandomBalanceAjustmentAmount, -minRandomBalanceAjustmentAmount));
+            targetBalance += DontGoOverMax(Random.Range(-MaxRandomBalanceAjustmentAmount, -minRandomBalanceAjustmentAmount));
         }
 
         // Natural fallover
-        currentBalance += DontGoOverMax(falloverMultiplyer * currentBalance * Time.deltaTime);
+        targetBalance += DontGoOverMax(falloverMultiplyer * targetBalance * Time.deltaTime);
 
         // Make it moar difficult!
         falloverMultiplyer += 0.05f * Time.deltaTime;
 
-        testSlider.value = currentBalance;
+        currentBalance = Mathf.SmoothDamp(currentBalance, targetBalance, ref smoothMovementVelocity, falloverSpeed);
+
+        testSliderCurrent.value = currentBalance;
+        testSliderTarget.value = targetBalance;
     }
 
     public void Undie()
     {
         currentBalance = 0;
+        targetBalance = 0;
         falloverMultiplyer = 1;
     }
 
     private float DontGoOverMax(float valueToClamp)
     {
-        float output = Mathf.Clamp(valueToClamp, -balanceMax - currentBalance, balanceMax - currentBalance);
+        float output = Mathf.Clamp(valueToClamp, (-balanceMax - 10) - targetBalance, (balanceMax + 10) - targetBalance);
         Debug.Assert(Mathf.Abs(output) < balanceMax);
         return output;
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -73,7 +73,6 @@ public class GameManager : MonoBehaviour
         PlayerManager.Undie();
         BalanceBarManager.Undie();
         Camera.main.transform.rotation = defaultCameraPosition;
-
     }
 
     public enum GameState
