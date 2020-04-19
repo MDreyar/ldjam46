a6dc9dcf9228b207aab3823fa204c1cc5b2ebcba
TheGravyNator <cpc.slabbekoorn@gmail.com>
2020-04-19 12:18:32 +0200

    Changed Zazo prefab and added delet wall

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Delete")
+        {
+            Debug.Log("DESU!");
+            Destroy(gameObject);
+        }
+    }
+}
